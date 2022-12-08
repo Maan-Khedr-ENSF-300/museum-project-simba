b22757f21ee97fb72541ec97f45ff68aea987e9b
sneha031 <113272607+sneha031@users.noreply.github.com>
2022-12-07 22:23:32 -0700

    Admin User functionality

--- Python_Files/Index.py
+++ Python_Files/Index.py
@@ -6,20 +6,25 @@ if __name__ == '__main__':
 
     print("\n********** Welcome to  Museum Management System ************\n")
     valid_user=functions.connect_database()
+    #print(valid_user)
 
 while (True):
     #Display Menu options
-    s=functions.display_Type_of_Users()
+    if valid_user==3:
+        s="3"
+    else:
+        s=functions.display_Type_of_Users()
 
-    print("**The value chosen is: ",s)
     if(s == "1"):
+        print("***Admin User Menu***\n")
         while (True):
             admin_op=functions.admin_User()
             if (admin_op=="1"):
-                print("Execute a SQL Script")
+                print("Execute a SQL Commands & Queries")
                 functions.admin_execute_query()
             elif(admin_op=="2"):
-                print("Execute a SQL Command")
+                print("Execute a SQL Script File")
+                functions.admin_execute_SqlFile()
             elif (admin_op == "3"):
                 print("User Access Management")
                 functions.user_access_mgmt()
@@ -30,10 +35,10 @@ while (True):
                 print("Please enter a valid key...")
             print("\n")
     elif(s == "2"):
-        print("You Selected Data Entry User Role")
+        print("***Data Entry User Menu***\n")
         functions.dataEntery_Option()
     elif(s == "3"):
-        print("You Selected End User or Browse-Only User Role\n")
+        print("***Browse/Guest User Menu***\n")
         functions.Enduser_BrowseOnly()
     elif(s == "4"):
         print("Quitting Program....")
@@ -42,5 +47,6 @@ while (True):
     else:
         print("Please enter a valid key...")
     print("\n")
+
 
 
